66a7261775edc348d477183e424056badc2f6b24
Xiaocao <wdjoys@hotmail.com>
2020-08-27 11:18:47 +0800

    1

--- NexuxPHP/dailyAttendance.py
+++ NexuxPHP/dailyAttendance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import os
 from config import COOKIE, HEADER, URL
 import requests
 import re
@@ -13,9 +14,12 @@ session.cookies.update(COOKIE)
 
 attendanceUrl = URL + '/attendance.php'
 
+
 with session.get(URL) as res:
     r = re.compile(r'签到已得\d+')
     tip = r.search(res.text).group() if r.search(res.text) else '也许、、、'
     timeArray = time.localtime()
-    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    otherStyleTime = time.strftime(r"%Y-%m-%d,%H:%M:%S", timeArray)
     print(otherStyleTime, tip)
+
+print(os.environ["COOKIE"])
