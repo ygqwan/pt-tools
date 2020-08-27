e2a6dd3fdb2e9d5f8480a4fc33199c3ecc22b65f
Xiaocao <wdjoys@hotmail.com>
2020-08-27 11:45:26 +0800

    11

--- NexuxPHP/config.py
+++ NexuxPHP/config.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-URL = 'https://www.hddolby.com'
+from NexuxPHP.dailyAttendance import COOKIE
 
-COOKIE = {
-    '__cfduid': 'd141b32015da2019a28d3505509faf6ef1598487660',
-    'cf_clearance': 'efc4cbe36e961b08b289e493870d99eb8b7cddfc-1598487660-0-1zde10b524zc599e4e5z15b516f6-150',
-    'c_secure_uid': 'MjEwMTA%3D',
-    'c_secure_pass': 'a7b591404cda1eb1ce113e26230988db',
-    'c_secure_ssl': 'eWVhaA%3D%3D',
-    'c_secure_tracker_ssl': 'bm9wZQ%3D%3D',
-    'c_secure_login': 'bm9wZQ % 3D % 3D'
-}
+
+URL = 'https://www.hddolby.com'
 
 HEADER = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'
 }
+
+# COOKIE JSON 格式放入 github 仓库 Secrets中
