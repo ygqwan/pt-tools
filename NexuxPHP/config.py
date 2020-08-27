357bf1ea6df6b971702ecfcb33f4775d0b0c3c7c
Xiaocao <wdjoys@hotmail.com>
2020-08-27 10:31:47 +0800

    1

--- NexuxPHP/config.py
+++ NexuxPHP/config.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+URL = 'https://www.hddolby.com'
+
 COOKIE = {
     '__cfduid': 'd141b32015da2019a28d3505509faf6ef1598487660',
     'cf_clearance': 'efc4cbe36e961b08b289e493870d99eb8b7cddfc-1598487660-0-1zde10b524zc599e4e5z15b516f6-150',
--- NexuxPHP/dailyAttendance.py
+++ NexuxPHP/dailyAttendance.py
@@ -1,1 +1,21 @@
-from requests import Request
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from config import COOKIE, HEADER, URL
+import requests
+import re
+import time
+
+session = requests.session()
+session.headers.update(HEADER)
+session.cookies.update(COOKIE)
+
+
+attendanceUrl = URL + '/attendance.php'
+
+with session.get(URL) as res:
+    r = re.compile(r'签到已得\d+')
+    tip = r.search(res.text).group() if r.search(res.text) else '也许、、、'
+    timeArray = time.localtime()
+    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    print(otherStyleTime, tip)
